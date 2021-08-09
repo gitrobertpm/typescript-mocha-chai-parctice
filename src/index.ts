703682b4eef785b24f2d7085554d11a91af2a7c5
robertpm <grobertpm@gmail.com>
2021-08-08 22:52:27 -0700

    Added axios for requests and started playing  with some typescript and some GET requests

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,54 @@
+
 import express from "express";
+import axios from 'axios';
 
 const app = express();
-const port = 8080;
+const port: number = 8080;
 
-app.get("/", (req, res) => {
-    res.send("Hello world!");
+
+// const asyncHandler = async (cb) => {
+//   return async (req, res, next) => {
+//     try {
+//       await cb(req, res, next);
+//     } catch(err) {
+//       console.log('err test: ', err);
+//       next(err);
+//     }
+//   }
+// }
+
+
+// const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
+const url = 'https://www.netflix.com/';
+
+// const makeGetReq = async (url: string) => {
+
+//   const getReq = await axios.get(url);
+
+//   console.log();
+
+//   // .then(res => {
+//   //     console.log(res.data.url);
+//   //     console.log(res.data.explanation);
+//   // })
+//   // .catch(err => {
+//   //     console.log(err);
+//   // });
+// }
+
+
+
+
+
+app.get("/", async (req, res) => {
+  const getRes = await axios.get(url);
+  // const formattedRes = JSON.stringify(getRes, null, 2)
+
+  console.log(getRes);
+  // console.log(formattedRes);
+  res.send(`Hello world! \n ${getRes}`);
 });
 
 app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
-}); 
+  console.log(`server started at http://localhost:${port}`);
+});
