3c83dcf428d84c29fb82c03a0c1f51990a18750b
robertpm <grobertpm@gmail.com>
2021-08-11 21:07:29 -0700

    First typsecript use complete

--- src/index.ts
+++ src/index.ts
@@ -2,52 +2,51 @@
 import express from "express";
 import axios from 'axios';
 
-const app = express();
+const app = express(); console.log(typeof app);
 const port: number = 8080;
 
+// Helper middleware for wrapping async functions in a try catch block
+const asyncHandler: Function = (cb: Function) => {
+  return async (req: Request, res: Response, next: Function) => {
+    try {
+      await cb(req, res, next);
+    } catch(err) {
+      console.log('ERR LOG: ', err);
+      next(err);
+    }
+  }
+};
 
-// const asyncHandler = async (cb) => {
-//   return async (req, res, next) => {
-//     try {
-//       await cb(req, res, next);
-//     } catch(err) {
-//       console.log('err test: ', err);
-//       next(err);
-//     }
-//   }
-// }
-
-
-// const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
-const url = 'https://www.netflix.com/';
-
-// const makeGetReq = async (url: string) => {
-
-//   const getReq = await axios.get(url);
-
-//   console.log();
-
-//   // .then(res => {
-//   //     console.log(res.data.url);
-//   //     console.log(res.data.explanation);
-//   // })
-//   // .catch(err => {
-//   //     console.log(err);
-//   // });
-// }
+// URL for GET request
+const url = 'https://teamtreehouse.com/robertpm.json';
 
 
 
+const makeGetReq: Function = async (url: string) => {
+
+  const getReq = await axios.get(url);
+
+  const result = {
+    name: getReq.data.name,
+    badges: getReq.data.badges.length,
+    points: getReq.data.points.total
+  }
+
+  return result;
+};
 
 
-app.get("/", async (req, res) => {
-  const getRes = await axios.get(url);
-  // const formattedRes = JSON.stringify(getRes, null, 2)
+interface IRes {
+  send: Function
+}
 
-  console.log(getRes);
-  // console.log(formattedRes);
-  res.send(`Hello world! \n ${getRes}`);
-});
+
+app.get("/", asyncHandler(async (req: Request, res: IRes, next: Function) => {
+
+  const getRes: {name: string, badges: number, points: string} = await makeGetReq(url);
+  const resStr: string = `<h1>Hello world!</h1> <p>Name: ${getRes.name}</p><p>Badge Count: ${getRes.badges}</p><p>Total Points: ${getRes.points}</p>`;
+  res.send(resStr);
+}));
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
