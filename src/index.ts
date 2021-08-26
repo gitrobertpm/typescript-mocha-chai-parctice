2745242c44569c406efad2d9d263da0aa7a185d8
robertpm <grobertpm@gmail.com>
2021-08-25 23:11:43 -0700

    Updated to render Pug templates

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ const app = express();
 const port: number = 8080;
 
 app.set('view engine', 'pug');
-app.use('/static', express.static(path.join(__dirname, 'public')));
-
+app.use('/static', express.static(path.join(__dirname.slice(0, -3), 'public')));
+console.log('DIRNAME: ', __dirname.slice(0, -3));
 
 // Title-ize a string
 export const titleCase: Function = (str: string) => {
@@ -80,15 +80,22 @@ const makeGetReq: Function = async (url: string) => {
 
 
 interface IRes {
-  send: Function
+  render: Function
 }
 
 
 app.get("/", asyncHandler(async (req: Request, res: IRes, next: Function) => {
 
   const getRes: {name: string, badges: number, points: string} = await makeGetReq(url);
-  const resStr: string = `<h1>Hello world!</h1> <p>Name: ${getRes.name}</p><p>Badge Count: ${getRes.badges}</p><p>Total Points: ${getRes.points}</p>`;
-  res.send(resStr);
+  // const resStr: string = `<h1>Hello world!</h1> <p>Name: ${getRes.name}</p><p>Badge Count: ${getRes.badges}</p><p>Total Points: ${getRes.points}</p>`;
+
+  const resData: {} = {
+    name: getRes.name,
+    badgeCount: getRes.badges,
+    totalPoints: getRes.points
+  }
+
+  res.render('index', {'thData': resData});
 }));
 
 app.listen(port, () => {
